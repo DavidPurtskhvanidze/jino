d2d3788fcce953a028d6fb2e10af3880344e211e
david <david.purtskhvanidze@gmail.com>
2019-03-22 02:52:58 +0600

    gulp set

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,32 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoPrefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
 
-gulp.task('default', function (done) {
-    gulp.src('app/css/main.css').pipe(gulp.dest('demo/css/'));
-    done();
+
+// gulp.task('serve', ['sass'], function() {
+//
+//     browserSync.init({
+//         server: "app/"
+//     });
+//
+//     gulp.watch("app/sass/*.sass", ['sass']);
+//     gulp.watch("app/*.html").on('change', browserSync.reload);
+// });
+
+// gulp.task('sass', function (done) {
+//     gulp.src('app/sass/style.sass')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(autoPrefixer())
+//         .pipe(gulp.dest('app/css'));
+//     done();
+// });
+
+gulp.task('sass', function() {
+    return gulp.src("app/sass/*.sass")
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('app/css'));
 });
+
+// gulp.task('default', ['serve']);
